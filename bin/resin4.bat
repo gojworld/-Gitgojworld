ac3fd4bc4ee4d8bb6bc44310fd5815ad97b452a7
joo <gojworld@syworks.com>
2015-11-24 10:54:30 +0900

    2015-11-24 NEW JAVASCRIPT

--- bin/resin4.bat
+++
@@ -1,4 +0,0 @@
-taskkill /F /IM "java.exe"
-taskkill /F /IM "javaw.exe"
-
-java.exe -jar  D:\Project\bin\resin-4.0.37\lib\resin.jar -conf D:\project\syworks\hanyang_mach_engrg_sb\bin\resin4.xml console
--- bin/start.bat
+++
@@ -1,4 +0,0 @@
-taskkill /F /IM "java.exe"
-taskkill /F /IM "javaw.exe"
-
-java.exe -jar  /Development/bin/resin-4.0.41/lib/resin.jar -conf C:\Development\workspaces\resin-web/bin/resin4.xml console
